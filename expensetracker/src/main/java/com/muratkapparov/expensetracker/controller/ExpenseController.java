8b04ab03dab506d219c79ac75b0c782cfcb1716e
Kapparov Murat <136073953+SOLIDEROFPOOP@users.noreply.github.com>
2024-05-16 10:03:57 +0600

    added: timestamp, http status

--- expensetracker/src/main/java/com/muratkapparov/expensetracker/controller/ExpenseController.java
+++ expensetracker/src/main/java/com/muratkapparov/expensetracker/controller/ExpenseController.java
@@ -22,11 +22,13 @@ public class ExpenseController {
     public Expense getExpenseById(@PathVariable("id") Long id){
         return expenseService.getExpenseById(id);
     }
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
     @DeleteMapping("/expenses")
     public void deleteExpenseById(@RequestParam("id") Long id){
         expenseService.deleteExpenseById(id);
     }
 
+    @ResponseStatus(value = HttpStatus.CREATED)
     @PostMapping("/expenses")
     public Expense saveExpenseDetails(@RequestBody Expense expense){
         return expenseService.saveExpenseDetails(expense);
@@ -35,4 +37,5 @@ public class ExpenseController {
     public Expense updateExpenseDetails(@RequestBody Expense expense, @PathVariable() Long id){
         return expenseService.updateExpenseDetails(id,expense);
     }
+
 }
--- expensetracker/src/main/java/com/muratkapparov/expensetracker/entity/Expense.java
+++ expensetracker/src/main/java/com/muratkapparov/expensetracker/entity/Expense.java
@@ -4,9 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
 import java.sql.Date;
+import java.sql.Timestamp;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -26,4 +30,12 @@ public class Expense {
     private String category;
 
     private Date date;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    private Timestamp createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private Timestamp updatedAt;
 }
